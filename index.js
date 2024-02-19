0d26a5cfff851afb36cbaa8f080322858ade16b7
Taswoor2507 <taswoorhossein72@gmail.com>
2024-02-19 16:43:56 +0500

    write express basic code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.json("https://fakestoreapi.com/products");
+});
+
+const port = process.env.PORT;
+console.log(port);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
